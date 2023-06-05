e31c33aa4f2343443d4f755afb7cf6de4a52f70e
ankush-003 <anshhv2003@gmail.com>
2023-06-05 22:17:27 +0530

    added gitignore and README

--- main.py
+++ main.py
@@ -47,3 +47,44 @@ class User(BaseModel):
     email: str or None = None
     full_name: str or None = None
     disabled: bool or None = None
+
+class UserInDB(User):
+    hashed_pwd: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+coauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+# the password is hashed and stored in the database, later when the user tries to login the password is hashed and compared with the hashed password in the database
+def verify_password(plain_pwd, hashed_pwd):
+    return pwd_context.verify(plain_pwd, hashed_pwd)
+
+def get_password_hash(pwd):
+    return pwd_context.hash(pwd)
+
+def get_user(db, username: str):
+    if username in db:
+        user_data = db[username]
+        # as the model accepts only the fields in the model, we are converting the dict to a model
+        return UserInDB(**user_data)
+
+def authenticate_user(db, username: str, pwd: str):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(pwd, user.hashed_pwd):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+asyn def get_current_user(token: str = Depends(coauth2_scheme), db: dict = Depends(get_db)):
